4b51a11b43ab5426391089eed861dfef1ce73232
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-06 15:45:40 -0500

    adds hash function to HashTable class

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -21,8 +21,8 @@ class HashTable:
     """
 
     def __init__(self, capacity):
+        self.capacity = capacity
         # Your code here
-
 
     def get_num_slots(self):
         """
@@ -36,7 +36,6 @@ class HashTable:
         """
         # Your code here
 
-
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
@@ -45,16 +44,24 @@ class HashTable:
         """
         # Your code here
 
-
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
 
         Implement this, and/or DJB2.
         """
+        # 64-bit
+        offset_basis = 14695981039346656037
+        FNV_prime = 1099511628211
+
+        hash = offset_basis
+        for char in key.encode():
+            hash = hash ^ char
+            hash *= FNV_prime
+
+        return hash
 
         # Your code here
-
 
     def djb2(self, key):
         """
@@ -64,13 +71,12 @@ class HashTable:
         """
         # Your code here
 
-
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
+        # return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
@@ -83,7 +89,6 @@ class HashTable:
         """
         # Your code here
 
-
     def delete(self, key):
         """
         Remove the value stored with the given key.
@@ -93,7 +98,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
     def get(self, key):
         """
@@ -105,7 +109,6 @@ class HashTable:
         """
         # Your code here
 
-
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
@@ -114,7 +117,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
 
 if __name__ == "__main__":
