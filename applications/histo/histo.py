1a300089e9920142dbdb4d023e18815b9f2eae40
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-08 17:26:32 -0500

    completes histogram cache functions

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,15 @@
-# Your code here
+from word_counter import word_count
 
+cache = {}
+
+with open("robin.txt", "r") as robin:
+    for line in robin:
+        word_count(line, cache)
+
+# sort cache
+sorted_cache = list(cache.items())
+sorted_cache.sort(key=lambda x: x[1], reverse=True)
+
+if __name__ == "__main__":
+    for word, count in sorted_cache:
+        print(f"{word.ljust(20)}{'#'*count}")
