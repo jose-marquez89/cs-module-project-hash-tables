ad15006511d4210dd723f7cd5de11f40f29a6910
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-08 16:48:11 -0500

    completes word count function

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,28 @@
-def word_count(s):
-    # Your code here
+import re
 
+
+def word_count(s):
+    cache = {}
+
+    # format string
+    s = s.lower()
+    pattern = re.compile(r"[^a-zA-Z\'\s]")
+    s = re.sub(pattern, "", s)
+    s = s.split()
+    print(s)
+
+    for word in s:
+        if word in cache:
+            cache[word] += 1
+        else:
+            cache[word] = 1
+
+    return cache
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('This is a test of the emergency broadcast '
+                     'network. This is only a test.'))
