f958355fca897346bd1fc645ca3f465614568cdc
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-08 16:48:31 -0500

    removes cache parameter in function args

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,7 +1,7 @@
-c = {}
+cache = {}
 
 
-def expensive_seq(x, y, z, cache):
+def expensive_seq(x, y, z):
     trio = (x, y, z)
     if trio in cache:
         return cache[trio]
@@ -9,16 +9,16 @@ def expensive_seq(x, y, z, cache):
         cache[trio] = y + z
         return cache[trio]
     else:
-        cache[trio] = (expensive_seq(x-1, y+1, z, cache) +
-                       expensive_seq(x-2, y+2, z*2, cache) +
-                       expensive_seq(x-3, y+3, z*3, cache))
+        cache[trio] = (expensive_seq(x-1, y+1, z) +
+                       expensive_seq(x-2, y+2, z*2) +
+                       expensive_seq(x-3, y+3, z*3))
 
         return cache[trio]
 
 
 if __name__ == "__main__":
     for i in range(10):
-        a = expensive_seq(i*2, i*3, i*4, c)
+        a = expensive_seq(i*2, i*3, i*4)
         print(f"{i*2} {i*3} {i*4} = {a}")
 
-    print(expensive_seq(150, 400, 800, c))
+    print(expensive_seq(150, 400, 800))
