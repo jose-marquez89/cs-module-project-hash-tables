7b125ee1dd88a51189aa6c749035c9ee0f63fc66
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-09 13:31:22 -0500

    completes markov chain with caching

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -5,9 +5,44 @@ with open("input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
-# Your code here
+sequence_cache = {}
+words = words.split()
+
+# build cache
+for w in range(len(words)-1):
+    if words[w] in sequence_cache:
+        sequence_cache[words[w]].append(words[w+1])
+    else:
+        sequence_cache[words[w]] = [words[w+1]]
+
+# group stop and start words
+consolidated_groups = sequence_cache.keys()
+start = []
+for w in consolidated_groups:
+    if w.istitle():
+        start.append(w)
 
 
-# TODO: construct 5 random sentences
-# Your code here
+def print_nonsense(start_list, word_cache):
+    stop_punc = ('.', '?', '!', '"')
+    word = random.choice(start_list)
+    all_words = list(word_cache.keys())
+    stop = False
+    print(word, end=' ')
+    word = random.choice(all_words)
+    while not stop:
+        if word in start_list:
+            word = random.choice(all_words)
+            continue
+        elif word[-1] in stop_punc:
+            stop = True
+            print(word)
+        else:
+            print(word, end=' ')
+            word = random.choice(all_words)
+
+
+if __name__ == "__main__":
+    for i in range(5):
+        print_nonsense(start, sequence_cache)
 
