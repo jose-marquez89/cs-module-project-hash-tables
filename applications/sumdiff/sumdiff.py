56b32530d71ed0bc4274bddc78b968569dbb2cc8
jose-marquez89 <jlmarquez89@gmail.com>
2020-07-10 05:03:32 -0500

    creates first pass solution of iteration issue

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -11,5 +11,40 @@ q = (1, 3, 4, 7, 12)
 def f(x):
     return x * 4 + 6
 
-# Your code here
+func_cache = {}
 
+def sum_diff(nums):
+    # build cache
+
+    slots = [0, 0, 0, 0]
+    prev = 3
+    next = 1
+    position = 3
+    limit = 0
+
+    while slots[0] < len(nums):
+        combo = (nums[slots[0]], nums[slots[1]],
+                 nums[slots[2]], nums[slots[3]])
+        left = f(combo[0]) + f(combo[1])
+        right = f(combo[2]) + f(combo[3])
+        if left == right:
+            func_cache[combo] = (left, right)
+
+        if position == limit:
+            slots[position] = next
+            position = 3
+            limit += 1
+        else:
+            slots[position] = next
+            position -= 1
+            slots[prev] = next - 1
+            prev -= 1
+
+        if limit == len(slots):
+            next += 1
+            limit = 0
+
+        print(slots)
+
+if __name__ == '__main__':
+    sum_diff(q)
